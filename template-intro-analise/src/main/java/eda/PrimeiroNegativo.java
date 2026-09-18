cbc4c38d26de8f0567b2007797817c96f989fbfe
Thalles Saraiva <thallesgabrielbr12@outlook.com>
2026-09-18 20:54:53 -0300

    merge

--- template-intro-analise/src/main/java/eda/PrimeiroNegativo.java
+++ template-intro-analise/src/main/java/eda/PrimeiroNegativo.java
@@ -1,4 +1,34 @@
 package eda;
 
-public class PrimeiroNegativo {
+import java.util.*;
+
+class PrimeiroNegativo {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("alg time sample");
+        while (sc.hasNextLine()){
+            String linha = sc.nextLine().trim();
+
+            if (linha.isEmpty()){
+                continue;
+            }
+
+            String[] nums = linha.split("\\s+");
+            long start = System.nanoTime();
+
+            procuraNegativo(nums, 0);
+
+            long end = System.nanoTime();
+            long time = end - start;
+            System.out.println("\"ProcuraNegativo Recursivo \" " + time + " " + nums.length);
+        }
+    }
+
+    public static boolean procuraNegativo(String[] v, int i){
+        if (i>=v.length) return false;
+        int n = Integer.parseInt(v[i]);
+        if (n < 0) return true;
+        return procuraNegativo(v,i+1);
+    }
 }
